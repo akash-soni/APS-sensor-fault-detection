f6d3ca58354e02fa5437632050660dfd71b83920
akash-soni <akash.200287@gmail.com>
2023-06-20 16:00:39 +0530

    Validation completed

---
+++ sensor/components/data_transformation.py
@@ -0,0 +1,121 @@
+from sensor.utils.main_utils import read_yaml_file, write_yaml_file
+from sensor.exception import SensorException
+from sensor.logger import logging
+import pandas as pd
+import os, sys
+from sensor.constant.training_pipeline import TARGET_COLUMN
+import numpy as np
+from imblearn.combine import SMOTETomek
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import Pipeline
+from sensor.entity.artifact_entity import (
+    DataTransformationArtifact,
+    DataValidationArtifact,
+)
+from sensor.entity.config_entity import DataTransformationConfig
+from sensor.utils.main_utils import save_numpy_array_data, save_object
+from sensor.ml.model.estimator import TargetValueMapping
+
+
+class DataTransformation:
+    def __init__(self,data_validation_artifact: DataValidationArtifact, 
+                    data_transformation_config: DataTransformationConfig,):
+        """
+
+        :param data_validation_artifact: Output reference of data ingestion artifact stage
+        :param data_transformation_config: configuration for data transformation
+        """
+        try:
+            self.data_validation_artifact = data_validation_artifact
+            self.data_transformation_config = data_transformation_config
+
+        except Exception as e:
+            raise SensorException(e, sys)
+
+
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise SensorException(e, sys)
+
+
+    @classmethod
+    def get_data_transformer_object(cls)->Pipeline:
+        try:
+            robust_scaler = RobustScaler()
+            simple_imputer = SimpleImputer(strategy="constant", fill_value=0)
+            preprocessor = Pipeline(
+                steps=[
+                    ("Imputer", simple_imputer), #replace missing values with zero
+                    ("RobustScaler", robust_scaler) #keep every feature in same range and handle outlier
+                    ]
+            )
+            
+            return preprocessor
+
+        except Exception as e:
+            raise SensorException(e, sys) from e
+
+    def initiate_data_transformation(self,) -> DataTransformationArtifact:
+        try:
+            logging.info(f"reading train and test data from valid file path")
+            train_df = DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_df = DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
+            
+            #initialize preprocessor function
+            preprocessor = self.get_data_transformer_object()
+
+            logging.info(f"Mapping TARGET column values to 0s and 1s")
+            #training dataframe
+            input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN], axis=1)
+            target_feature_train_df = train_df[TARGET_COLUMN]
+            target_feature_train_df = target_feature_train_df.replace( TargetValueMapping().to_dict())
+
+            #testing dataframe
+            input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN], axis=1)
+            target_feature_test_df = test_df[TARGET_COLUMN]
+            target_feature_test_df = target_feature_test_df.replace(TargetValueMapping().to_dict())
+
+            logging.info(f"transforming data")
+            preprocessor_object = preprocessor.fit(input_feature_train_df)
+            transformed_input_train_feature = preprocessor_object.transform(input_feature_train_df)
+            transformed_input_test_feature =preprocessor_object.transform(input_feature_test_df)
+
+            logging.info(f"Balancing train and test data using SMOTETomek data")
+
+            smt = SMOTETomek(sampling_strategy="minority")
+
+            input_feature_train_final, target_feature_train_final = smt.fit_resample(
+                transformed_input_train_feature, target_feature_train_df
+            )
+
+            input_feature_test_final, target_feature_test_final = smt.fit_resample(
+                transformed_input_test_feature, target_feature_test_df
+            )
+
+            logging.info(f"Balancing done, Concatenating data arrays with target column")
+            train_arr = np.c_[input_feature_train_final, np.array(target_feature_train_final) ]
+            test_arr = np.c_[ input_feature_test_final, np.array(target_feature_test_final) ]
+
+            logging.info(f"Save the data as train and test arrays")
+            
+            #save numpy array data
+            save_numpy_array_data( self.data_transformation_config.transformed_train_file_path, array=train_arr, )
+            save_numpy_array_data( self.data_transformation_config.transformed_test_file_path,array=test_arr,)
+            save_object( self.data_transformation_config.transformed_object_file_path, preprocessor_object,)
+            
+            #preparing artifact
+            logging.info("prepare Transformation artifact")
+            data_transformation_artifact = DataTransformationArtifact(
+                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
+                transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path=self.data_transformation_config.transformed_test_file_path,
+            )
+            logging.info(f"Data transformation artifact saved at: {data_transformation_artifact}")
+            return data_transformation_artifact
+
+        except Exception as e:
+            raise SensorException(e, sys)
